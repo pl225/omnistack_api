1c488797f9cdde5a929f7d5838fc6725cab4190f
Matheus Abreu <matheusa596@gmail.com>
2020-03-01 15:27:06 -0300

    Configurando servidor para acessar rotas, mongodb e cors.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const cors = require('cors');
 
 const server = express();
 
-server.get('/', (req, res) => {
-    return res.json({ mensagem: `Haha ${req.query.nome}` });
+mongoose.connect('mongodb+srv://pl225:Pl2252122*@cluster0-mebc5.mongodb.net/omnistack?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
 
 server.listen(5000);
