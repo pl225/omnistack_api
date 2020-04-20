0fb289e20d2cf4f8cc70e89fe6281c35b352b4f6
Matheus Abreu <matheusa596@gmail.com>
2020-04-20 06:50:52 -0300

    Tratando exceções

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -4,53 +4,62 @@ const Dev = require('../models/Dev');
 module.exports = {
     async index(req, res) {
         
-        const { user } = req.headers;
+        try {
+            const { user } = req.headers;
 
-        const loggedUser = await Dev.findById(user);
+            const loggedUser = await Dev.findById(user);
 
-        const users = await Dev.find({
-            $and: [
-                {
-                    _id: {
-                        $ne: loggedUser._id
+            const users = await Dev.find({
+                $and: [
+                    {
+                        _id: {
+                            $ne: loggedUser._id
+                        }
+                    },
+                    {
+                        _id: {
+                            $nin: loggedUser.likes
+                        }
+                    },
+                    {
+                        _id: {
+                            $nin: loggedUser.dislikes
+                        }
                     }
-                },
-                {
-                    _id: {
-                        $nin: loggedUser.likes
-                    }
-                },
-                {
-                    _id: {
-                        $nin: loggedUser.dislikes
-                    }
-                }
-            ]
-        });
+                ]
+            });
 
-        res.json(users);
+            res.json(users);
+        } catch (e) {
+            res.status(500).json(e);
+        }
     },
     async store(req, res) {
 
-        const { username } = req.body;
+        try {
+            const { username } = req.body;
 
-        const user = await Dev.findOne({ user: username });
+            const user = await Dev.findOne({ user: username });
 
-        if (user) {
-            return res.json(user);
+            if (user) {
+                return res.json(user);
+            }
+
+            const response = await axios.get(`https://api.github.com/users/${username}`);
+
+            const { name, bio, avatar_url } = response.data; 
+
+            const dev = await Dev.create({
+                name,
+                user: username,
+                bio,
+                avatar: avatar_url
+            });
+
+            return res.json(dev);
+
+        } catch (e) {
+            return res.status(500).json(e);
         }
-
-        const response = await axios.get(`https://api.github.com/users/${username}`);
-
-        const { name, bio, avatar_url } = response.data; 
-
-        const dev = await Dev.create({
-            name,
-            user: username,
-            bio,
-            avatar: avatar_url
-        });
-
-        return res.json(dev);
     }
 }
